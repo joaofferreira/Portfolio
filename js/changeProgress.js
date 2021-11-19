5b1f24a00131a06b64087b20175edbdfc3850166
joaofferreira <94402013+joaofferreira@users.noreply.github.com>
2021-11-19 11:41:24 +0000

    progress bars

--- js/changeProgress.js
+++ js/changeProgress.js
@@ -1,22 +1,34 @@
 var skillsIcons = document.querySelectorAll(".box-skills>i");
+var progressBars = document.querySelectorAll(".progress-bar");
+var resumeBtn = document.getElementById("resumeBtn");
 
 //animates the elements as they appear
 function animations() {
-  
-    if (isInViewport(hrSkills)) {
+  if (isInViewport(hrSkills)) {
     hrSkills.classList.add("separator-line");
-    }else if(isInViewport(hrAbout)){
-        hrAbout.classList.add("separator-line");
-    }else if(isInViewport(hrProjects)){
-        hrProjects.classList.add("separator-line");
-    }
-
+  } else if (isInViewport(hrAbout)) {
+    hrAbout.classList.add("separator-line");
+  } else if (isInViewport(hrProjects)) {
+    hrProjects.classList.add("separator-line");
+  }
+  if (isInViewport(resumeBtn)) {
+    resumeBtn.classList.add("shake");
+  }
   skillsIcons.forEach((element) => {
-      if(isInViewport(element)){
-        element.classList.add("scale");
-      }
+    if (isInViewport(element)) {
+      element.classList.add("scale");
+    }
   });
-  
+
+  progressBars.forEach((element) => {
+    if (isInViewport(element)) {
+      str = element.getAttribute("width");
+      width = str.match(/\d+/g)[0];
+        element.classList.add("progressBar");
+        element.style.setProperty('--w', width+'%');
+
+    }
+  });
 }
 
 function changeProgress() {
